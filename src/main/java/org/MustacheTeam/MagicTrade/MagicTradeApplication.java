b865b81fc4dc70ac707273ad03ca9021e24e6e81
hannahazan <hannahazan2@gmail.com>
2025-05-14 15:14:33 +0200

    spring initializer

---
+++ src/main/java/org/MustacheTeam/MagicTrade/MagicTradeApplication.java
@@ -0,0 +1,13 @@
+package org.MustacheTeam.MagicTrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicTradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicTradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/MustacheTeam/MagicTrade/MagicTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.MustacheTeam.MagicTrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
